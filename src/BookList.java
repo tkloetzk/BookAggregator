ce8ed7ad7734b8d844031a5420a1bca38f28b280
Tucker Kloetzke <tuckerkloetzke@gmail.com>
2018-06-13 17:47:53 -0500

    More usage of BookList

--- src/BookList.java
+++ src/BookList.java
@@ -5,10 +5,26 @@ import java.util.List;
 public class BookList {
 
 	private Book[] Book;
-	private List<Book> books = new ArrayList<Book>();
+	private List<Book> books;
+	
+	public BookList() {
+		this.books = new ArrayList<Book>();
+	}
+	
+	public BookList(List<Book> bookList) {
+		this.books = bookList;
+	}
 
 	public Book getBook(int index) {
 		return books.get(index);
+	}
+	
+	public void removeBook(Book book) {
+		books.remove(book);
+	}
+	
+	public void removeBookByIndex(int index) {
+		books.remove(index);
 	}
 
 	public List<Book> getBooks() {
@@ -27,4 +43,5 @@ public class BookList {
 	public String toString() {
 		return "BookList [Book=" + Arrays.toString(Book) + ", books=" + books + "]";
 	}
+
 }
--- src/main.java
+++ src/main.java
@@ -7,15 +7,10 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -40,45 +35,46 @@ public class main {
 	private static String goodreadsURL = "https://www.goodreads.com/book/title.xml?key=yXZIGleYDqexQC7C40PFg&title=";
 	public static boolean include_subdirectories = true; //TODO change to false once working
 	public String base_directory;
-	public static List<Book> bookList;
-	public static BookList failedTitles = new BookList();
-	public static BookList succeededTitles = new BookList();
-	
+	public static BookList failedBooks = new BookList();
+	public static BookList bookList;
+
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-	//	System.out.print("Base directory name: ");
+		//	System.out.print("Base directory name: ");
 		//String base_folder = input.nextLine();
 		String base_folder ="Learnings";
 
 		//TODO Doesn't work
-//		System.out.print("Do you want to include subdirectories? (yes/no): ");
-//		String yes_no = input.nextLine();
-//		if (Character.toLowerCase(yes_no.charAt(0)) == 'y') {
-//			include_subdirectories = true;
-//		}
+		//		System.out.print("Do you want to include subdirectories? (yes/no): ");
+		//		String yes_no = input.nextLine();
+		//		if (Character.toLowerCase(yes_no.charAt(0)) == 'y') {
+		//			include_subdirectories = true;
+		//		}
 
 		try {
-			bookList = Files.walk(Paths.get("/Users/Tucker/Documents/" + base_folder), FileVisitOption.FOLLOW_LINKS)
+			List<Book> files = Files.walk(Paths.get("/Users/Tucker/Documents/" + base_folder), FileVisitOption.FOLLOW_LINKS)
 					.filter(p -> (p.toString().endsWith(".pdf") || p.toString().endsWith(".epub")))
 					.map(fields -> 
-						new Book(FilenameUtils.removeExtension(fields.getFileName().toString()), 
-								fields.getParent().toString().substring(fields.getParent().toString().lastIndexOf('/') + 1).trim(),
-								FilenameUtils.getPath(fields.toAbsolutePath().toString()),
-								FilenameUtils.getExtension(fields.toAbsolutePath().toString())))
+					new Book(FilenameUtils.removeExtension(fields.getFileName().toString()), 
+							fields.getParent().toString().substring(fields.getParent().toString().lastIndexOf('/') + 1).trim(),
+							FilenameUtils.getPath(fields.toAbsolutePath().toString()),
+							FilenameUtils.getExtension(fields.toAbsolutePath().toString())))
 					.collect(Collectors.toList()); //TODO Save this to BookList
+
+			bookList = new BookList(files);
 
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-			
+
 		createGoodreadsThreads(bookList);
 
-		if (failedTitles.getNumberOfBooks() > 0) {
-			System.out.println(failedTitles.toString());
-			
-			System.out.print(failedTitles.getNumberOfBooks() + " failed. Do you want to edit the filenames and try again? ");
-			
+		if (failedBooks.getNumberOfBooks() > 0) {
+			System.out.println(failedBooks.toString());
+
+			System.out.print(failedBooks.getNumberOfBooks() + " failed. Do you want to edit the filenames and try again? ");
+
 			var editFailedFiles = false;
 			if (Character.toLowerCase(input.next().charAt(0)) == 'y') {
 				editFailedFiles = true;
@@ -86,24 +82,26 @@ public class main {
 			if (editFailedFiles) {
 				editFailedFiles();
 			}
-		//	createGoodreadsThreads(test);
 			
-			System.out.println(failedTitles.toString());
+			createGoodreadsThreads(failedBooks);
+
+			System.out.println(failedBooks.toString());
 		}
-		
+
 		input.close();
-		
+
 	}
 
-	private static void createGoodreadsThreads(List<Book> titles) { // change to generic
-		Thread[] threads = new Thread[titles.size()];
+	private static void createGoodreadsThreads(BookList bookList) { // change to generic
+		Thread[] threads = new Thread[bookList.getNumberOfBooks()];
 		PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
 		cm.setMaxTotal(5); // increase max total connection to 20
 		cm.setDefaultMaxPerRoute(5); // increase max connection per route to 20
 		RequestConfig localConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD).build();
 		CloseableHttpClient httpClient = HttpClients.custom().setConnectionManager(cm).build();
+
 		for (int i = 0; i < threads.length; i++) {
-			String title = titles.get(i).getTitle();
+			String title = bookList.getBook(i).getTitle();
 			HttpGet httpgetGoodreads;
 			try {
 				httpgetGoodreads = new HttpGet(goodreadsURL + URLEncoder.encode(title, "UTF-8"));
@@ -111,7 +109,7 @@ public class main {
 				httpgetGoodreads = new HttpGet(goodreadsURL + title);
 			}
 			httpgetGoodreads.setConfig(localConfig);
-			threads[i] = new Thread(new main().new GetGoodreadsTask(httpClient, httpgetGoodreads, titles.get(i)));
+			threads[i] = new Thread(new main().new GetGoodreadsTask(httpClient, httpgetGoodreads, bookList.getBook(i)));
 			threads[i].start();
 		}
 		// wait for all the threads to finish
@@ -124,31 +122,32 @@ public class main {
 			}
 		}
 	}
-	
+
 	private static void editFailedFiles() {
 		try {
 			BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-//			for (var i = 0; i < failedTitles.size(); i++) {
+			//			for (var i = 0; i < failedTitles.size(); i++) {
 			for (var i = 0; i < 2; i++) {
-				Book failedBook = failedTitles.getBook(i);
+				Book failedBook = failedBooks.getBook(i);
 				System.out.println("Edit title: " + failedBook.getTitle());
 				try {
 					String renamedTitle = input.readLine();
 					if (renamedTitle.trim().length() > 0) {
 						File file = new File(failedBook.getFullPath() + failedBook.getExt());
-				        File newFile = new File(failedBook.getPath() + renamedTitle + failedBook.getExt());
-				        if (file.renameTo(newFile)){
-				            System.out.println("File rename success");;
-				        } else {
-				            System.out.println("File rename failed");
-				        }
+						File newFile = new File(failedBook.getPath() + renamedTitle + failedBook.getExt());
+						if (file.renameTo(newFile)){
+							failedBook.setTitle(renamedTitle);
+							System.out.println("File rename success");
+						} else {
+							System.out.println("File rename failed");
+						}
 					}
 				} catch (IOException e) {
 					System.out.println("Failed to read input");
 					e.printStackTrace();
 				}
-				
-	
+
+
 			}
 			input.close();
 		} catch (IOException e) {
@@ -161,7 +160,7 @@ public class main {
 		private final HttpContext context;
 		private final HttpGet httpget;
 		private Book book;
-		
+
 		public GetGoodreadsTask(CloseableHttpClient httpClient, HttpGet httpget, Book book) {
 			this.httpClient = httpClient;
 			this.context = HttpClientContext.create();
@@ -208,20 +207,19 @@ public class main {
 				System.out.println(title + " has " + ratings_count + " ratings with an average rating of " + average_rating);
 				book.setAverage_rating(Double.parseDouble(average_rating));
 				book.setRatings_count(Double.parseDouble(ratings_count));
-				succeededTitles.addBook(book);
+				//succeededTitles.addBook(book);
 			} catch (NullPointerException e) {
-				failedTitles.addBook(book);
-				//System.out.println("Failed to get title: " + bookTitle);
+				failedBooks.addBook(book);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 
-//		private void saveBook(String title, String average_rating, String ratings_count) {
-//			book = new Book(title, Double.parseDouble(ratings_count), Double.parseDouble(average_rating));
-//		//	books.addBook(book);
-//		}
-		
+		//		private void saveBook(String title, String average_rating, String ratings_count) {
+		//			book = new Book(title, Double.parseDouble(ratings_count), Double.parseDouble(average_rating));
+		//		//	books.addBook(book);
+		//		}
+
 		public Book getBook() {
 			return book;
 		}
